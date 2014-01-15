7cb68b857305aaecb8e996797d22b92089c73753
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-15 23:03:54 +0900

    BaseballGameTest - test case 작성

---
+++ src/test/java/baseball/BaseballGameTest.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 1. 15.
+ * Time: 오후 11:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseballGameTest {
+	@Test
+	public void nothing() {
+
+	}
+}
