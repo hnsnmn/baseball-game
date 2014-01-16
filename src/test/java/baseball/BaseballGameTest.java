8d75754742ea0a19a77b4a31aa6b0422229a834b
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-16 22:22:32 +0900

    BaseballGameTest - add failing test for givenExactMatchingGuessNum_returnSolvedResult();

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.fail;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,14 +25,26 @@ public class BaseballGameTest {
 
 	@Test
 	public void givenInvalideGuessNumber_throwIllegalArgEx() {
+		// 값이 비 정상적인 테스트 케이스
 		assertIllegalArgExThrown(null);
 		assertIllegalArgExThrown("12");
+
 		assertIllegalArgExThrown("12a");
 		assertIllegalArgExThrown("a45");
+
 		assertIllegalArgExThrown("113");
 		assertIllegalArgExThrown("011");
 		assertIllegalArgExThrown("141");
 	}
+
+	@Test
+	public void givenExactMatchingGuessNum_returnSolvedResult() {
+		assertThat(guessResult.isSolved(), is(true));
+		assertThat(guessResult.isStrike(), is(3));
+		assertThat(guessResult.isBalls(), is(0));
+	}
+
+
 
 	private void assertIllegalArgExThrown(String guessNum) {
 		try {
